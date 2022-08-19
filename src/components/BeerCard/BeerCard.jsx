08df8bfea9296014d5e58dbbba47dc6b4bbcf9ab
jmmascorro <jessemascorro88@gmail.com>
2022-08-19 11:37:48 -0500

    added a button to the beer card to be able to click and render new information regarding each beer you click

--- src/components/BeerCard/BeerCard.jsx
+++ src/components/BeerCard/BeerCard.jsx
@@ -1,16 +1,42 @@
 import "./BeerCard.scss";
+import { useState } from "react";
 
-const BeerCard = ({ name, image, tagline, brewed, abv }) => {
-    return (
-        <div className="beer-card">
-          <h2 className="beer-card__name">{name}</h2>
-          <img className="beer-card__image" src={image} alt={name} />
-          <h2 className="beer-card__tagline">{tagline}</h2>
-          <div>
-            <button>More Info</button>
-            </div>
-        </div>
-      );
+const BeerCard = ({ name, image, tagline, description, abv, ph, brewed }) => {
+  
+  const [showMoreInfo, setShowMoreInfo] = useState(false);
+
+  const toggleMoreInfo = () => {
+    setShowMoreInfo(!showMoreInfo);
+  }
+
+  if(showMoreInfo === false) {
+
+    return (  
+      <div className="beer-card">
+        <h2 className="beer-card__name">{name}</h2>
+        <img className="beer-card__image" src={image} alt={name} />
+        <h2 className="beer-card__tagline">{tagline}</h2>
+        <div>
+          <button onClick={toggleMoreInfo}>More Info</button>
+          </div>
+      </div>
+    )
+
+  }else {
+    return  (
+      <div className="beer-card">
+      <h2 className="beer-card__name">{name}</h2>
+        <h2 className="more-info-description">{description}</h2>
+        <h2 className="more-info-brewed">{brewed}</h2>
+        <h2 className="more-info-abv">{abv}</h2>
+        <h2 className="more-info-ph">{ph}</h2>
+        <div>
+          <button onClick={toggleMoreInfo}>Thank You</button>
+          </div>
+      </div>)
+  }
+
+    
 };
 
 export default BeerCard;
--- src/components/BeerContainer/BeerContainer.jsx
+++ src/components/BeerContainer/BeerContainer.jsx
@@ -5,17 +5,20 @@ const BeerContainer = (props) => {
 
     const { beers } = props;
 
-    const mappedBeers = beers.map((data) => {
+        const mappedBeers = beers.map((data) => {
         
-        return <BeerCard name={data.name} image={data.image_url} tagline={data.tagline} />
-    })
+            return (
+                <BeerCard name={data.name} image={data.image_url} brewed={data.first_brewed}
+                tagline={data.tagline} description={data.description} abv={data.abv} ph={data.ph}/> 
+            )
+            })
+    
+        return (
+            <div className="beers">
+                {mappedBeers}
+            </div>
+        )
 
-    return (
-        <div className="beers">
-            {mappedBeers}
-        </div>
-    )
-
-}
+    }
 
 export default BeerContainer;
